d59e86509fa96bd32146ab9e47d56fc334e611f1
Денис Жигалов <dzhig001@gmail.com>
2022-07-05 21:11:27 +0500

    В классе BNData создан метод проверки валидности IP адреса ValidIP.

--- BN/Models/BNData.cs
+++ BN/Models/BNData.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BN.Models
 {
@@ -15,6 +17,17 @@ namespace BN.Models
         /// </summary>
         /// <param name="ProportionalValue">Массив не шкалированных параметров</param>
         /// <returns>Массив шкаллированных данных</returns>
+        /// 
+        public bool ValidIPV4(string IPAdress)
+        {
+            
+            var Match = Regex.IsMatch(IPAdress, "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+            if (Match == false)
+            {
+                MessageBox.Show("IP адрес не соответствуте формату IPV4");
+            }
+            return Match;
+        }
         public double[] Scale(int[] ProportionalValue, ushort GatewayFullScaleValue, short LowerMonitorRange, short UpperMonitorRange, double FaultReplace)
         {
             double[] ScaledValue = new double[ProportionalValue.Length];
--- BN/ViewModels/Base/MainWindowViewModel.cs
+++ BN/ViewModels/Base/MainWindowViewModel.cs
@@ -245,104 +245,115 @@ namespace BN.ViewModels.Base
             cmd_ConToRack_ST6 = new RelayCommand(o =>
             {
                 bt_Discon_ST6 = false;
+                var bnRack_ST6 = new BNData();
+                if (bnRack_ST6.ValidIPV4(tb_AdressIP_ST6) == true)
+                {
+                    var modbusClient_ST6 = new ModbusClient(tb_AdressIP_ST6, 502);
 
-                 var modbusClient_ST6 = new ModbusClient(tb_AdressIP_ST6, 502);
-
-                  // Проверка валидности IP адреса
-                  var Match = Regex.IsMatch(tb_AdressIP_ST6, "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
-                  if (Match == false)
-                  {
-                      MessageBox.Show("IP адрес не соответствуте формату IPV4");
-                      return;
-                  }
-
-                  try
-                  {
-                      modbusClient_ST6.Connect();
-                  }
-                  catch (Exception )
-                  { 
-                      MessageBox.Show("Устройство не отвечает");
-                      return;
-                  }    
-
-                      tbl_Status_ST6 = "Подключено";
-                      sb_Bckgrnd_ST6 = "LightGreen";
-                      bt_ConAct_ST6 = false;
-                      tb_IPAdrrAct_ST6 = false;
-                      bt_DisconAct_ST6 = true;
-
-                      var bnRack_ST6 = new BNData();
-                      var timer_ST6 = new DispatcherTimer(DispatcherPriority.Render);              
-                       timer_ST6.Interval = TimeSpan.FromSeconds(1);
-                       timer_ST6.Tick += (sender, args) =>
-                      {
-                          if (bt_Discon_ST6 == false)
-                          {
-                              int[] prm_st6_gr0 = modbusClient_ST6.ReadHoldingRegisters(5000, 23);
-                              double[] rtrn_prm_st6_gr0 = bnRack_ST6.Scale(prm_st6_gr0, GatewayFullScaleValue_ST6,
-                                                                            LowerMonitorRange_ST6_gr0, UpperMonitorRange_ST6_gr0,
-                                                                            FaultReplace_1);
-                              int[] prm_st6_gr1 = modbusClient_ST6.ReadHoldingRegisters(5024, 2);
-                              double[] rtrn_prm_st6_gr1 = bnRack_ST6.Scale(prm_st6_gr1, GatewayFullScaleValue_ST6, 
-                                                                            LowerMonitorRange_ST6_gr1, UpperMonitorRange_ST6_gr1,
-                                                                            FaultReplace_0);
-                              int[] prm_st6_gr5 = modbusClient_ST6.ReadHoldingRegisters(5042, 1);
-                              double[] rtrn_prm_st6_gr5 = bnRack_ST6.Scale(prm_st6_gr5, GatewayFullScaleValue_ST6,
-                                                                            LowerMonitorRange_ST6_gr5, UpperMonitorRange_ST6_gr5,
-                                                                            FaultReplace_0);
-                              int[] prm_st6_gr7 = modbusClient_ST6.ReadHoldingRegisters(5047, 1);
-                              double[] rtrn_prm_st6_gr7 = bnRack_ST6.Scale(prm_st6_gr7, GatewayFullScaleValue_ST6,
-                                                                            LowerMonitorRange_ST6_gr1, UpperMonitorRange_ST6_gr1,
-                                                                            FaultReplace_0);
-
-                              var visualEffects_ST6 = new VisualEffects();
-                              string[] bckgrd_st6_gr0 = visualEffects_ST6.LimitBrush_1(rtrn_prm_st6_gr0, WH_Y_0_ST6_gr0, WH_Y_1_ST6_gr0,
-                                                                                AH_R_0_ST6_gr0, AH_R_1_ST6_gr0,
-                                                                                UpperMonitorRange_ST6_gr0, LowerMonitorRange_ST6_gr0);
-                              string[] bckgrd_st6_gr1 = visualEffects_ST6.LimitBrush_0(rtrn_prm_st6_gr1, WH_Y_0_ST6_gr1, WH_Y_1_ST6_gr1,
-                                                                                WL_Y_0_ST6_gr1, WL_Y_1_ST6_gr1, AH_R_0_ST6_gr1,
-                                                                                AH_R_1_ST6_gr1, AL_R_0_ST6_gr1, AL_R_1_ST6_gr1,
-                                                                                UpperMonitorRange_ST6_gr1, LowerMonitorRange_ST6_gr1);
-                              string[] bckgrd_st6_gr5 = visualEffects_ST6.LimitBrush_0(rtrn_prm_st6_gr5, WH_Y_0_ST6_gr5, WH_Y_1_ST6_gr5,
-                                                                                WL_Y_0_ST6_gr5, WL_Y_1_ST6_gr5, AH_R_0_ST6_gr5,
-                                                                                AH_R_1_ST6_gr5, AL_R_0_ST6_gr5, AL_R_1_ST6_gr5,
-                                                                                UpperMonitorRange_ST6_gr5, LowerMonitorRange_ST6_gr5);
-                              string[] bckgrd_st6_gr7 = visualEffects_ST6.LimitBrush_0(rtrn_prm_st6_gr7, WH_Y_0_ST6_gr1, WH_Y_1_ST6_gr1,
-                                                                                WL_Y_0_ST6_gr1, WL_Y_1_ST6_gr1, AH_R_0_ST6_gr1,
-                                                                                AH_R_1_ST6_gr1, AL_R_0_ST6_gr1, AL_R_1_ST6_gr1,
-                                                                                UpperMonitorRange_ST6_gr1, LowerMonitorRange_ST6_gr1);
-
-                              prm_10MAD10CY011 = rtrn_prm_st6_gr0[0];
-                              prm_10MAD10CY012 = rtrn_prm_st6_gr0[2];
-                              prm_10MAD10CG010 = rtrn_prm_st6_gr1[0];
-                              prm_10MAD10CG011 = rtrn_prm_st6_gr1[1];
-                              prm_10MAD20CG010 = rtrn_prm_st6_gr5[0];
-                              prm_10MAD10CG012 = rtrn_prm_st6_gr7[0];
-
-                              bckgrd_10MAD10CY011 = bckgrd_st6_gr0[0];
-                              bckgrd_10MAD10CY012 = bckgrd_st6_gr0[2];
-                              bckgrd_10MAD10CG010 = bckgrd_st6_gr1[0];
-                              bckgrd_10MAD10CG011 = bckgrd_st6_gr1[1];
-                              bckgrd_10MAD20CG010 = bckgrd_st6_gr5[0];
-                              bckgrd_10MAD10CG012 = bckgrd_st6_gr7[0];
+                    try
+                    {
+                        modbusClient_ST6.Connect();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Устройство не отвечает");
+                        return;
+                    }
 
 
 
-                          }
-                          else
-                          {
-                              modbusClient_ST6.Disconnect();
-                              timer_ST6.Stop();
-                              tbl_Status_ST6 = "Отключено";
-                              sb_Bckgrnd_ST6 = "Coral";
-                              bt_ConAct_ST6 = true;
-                              tb_IPAdrrAct_ST6 = true;
-                              bt_DisconAct_ST6 = false;
 
-                          }
-};
-                   timer_ST6.Start();
+                    // Проверка валидности IP адреса
+                    //var Match = Regex.IsMatch(tb_AdressIP_ST6, "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+                    //if (Match == false)
+                    //{
+                    //    MessageBox.Show("IP адрес не соответствуте формату IPV4");
+                    //    return;
+                    //}
+
+
+                    tbl_Status_ST6 = "Подключено";
+                    sb_Bckgrnd_ST6 = "LightGreen";
+                    bt_ConAct_ST6 = false;
+                    tb_IPAdrrAct_ST6 = false;
+                    bt_DisconAct_ST6 = true;
+
+
+                    var timer_ST6 = new DispatcherTimer(DispatcherPriority.Render);
+                    timer_ST6.Interval = TimeSpan.FromSeconds(1);
+                    timer_ST6.Tick += (sender, args) =>
+                   {
+                       if (bt_Discon_ST6 == false)
+                       {
+                           int[] prm_st6_gr0 = modbusClient_ST6.ReadHoldingRegisters(5000, 23);
+                           double[] rtrn_prm_st6_gr0 = bnRack_ST6.Scale(prm_st6_gr0, GatewayFullScaleValue_ST6,
+                                                                         LowerMonitorRange_ST6_gr0, UpperMonitorRange_ST6_gr0,
+                                                                         FaultReplace_1);
+                           int[] prm_st6_gr1 = modbusClient_ST6.ReadHoldingRegisters(5024, 2);
+                           double[] rtrn_prm_st6_gr1 = bnRack_ST6.Scale(prm_st6_gr1, GatewayFullScaleValue_ST6,
+                                                                         LowerMonitorRange_ST6_gr1, UpperMonitorRange_ST6_gr1,
+                                                                         FaultReplace_0);
+                           int[] prm_st6_gr5 = modbusClient_ST6.ReadHoldingRegisters(5042, 1);
+                           double[] rtrn_prm_st6_gr5 = bnRack_ST6.Scale(prm_st6_gr5, GatewayFullScaleValue_ST6,
+                                                                         LowerMonitorRange_ST6_gr5, UpperMonitorRange_ST6_gr5,
+                                                                         FaultReplace_0);
+                           int[] prm_st6_gr7 = modbusClient_ST6.ReadHoldingRegisters(5047, 1);
+                           double[] rtrn_prm_st6_gr7 = bnRack_ST6.Scale(prm_st6_gr7, GatewayFullScaleValue_ST6,
+                                                                         LowerMonitorRange_ST6_gr1, UpperMonitorRange_ST6_gr1,
+                                                                         FaultReplace_0);
+
+                           var visualEffects_ST6 = new VisualEffects();
+                           string[] bckgrd_st6_gr0 = visualEffects_ST6.LimitBrush_1(rtrn_prm_st6_gr0, WH_Y_0_ST6_gr0, WH_Y_1_ST6_gr0,
+                                                                             AH_R_0_ST6_gr0, AH_R_1_ST6_gr0,
+                                                                             UpperMonitorRange_ST6_gr0, LowerMonitorRange_ST6_gr0);
+                           string[] bckgrd_st6_gr1 = visualEffects_ST6.LimitBrush_0(rtrn_prm_st6_gr1, WH_Y_0_ST6_gr1, WH_Y_1_ST6_gr1,
+                                                                             WL_Y_0_ST6_gr1, WL_Y_1_ST6_gr1, AH_R_0_ST6_gr1,
+                                                                             AH_R_1_ST6_gr1, AL_R_0_ST6_gr1, AL_R_1_ST6_gr1,
+                                                                             UpperMonitorRange_ST6_gr1, LowerMonitorRange_ST6_gr1);
+                           string[] bckgrd_st6_gr5 = visualEffects_ST6.LimitBrush_0(rtrn_prm_st6_gr5, WH_Y_0_ST6_gr5, WH_Y_1_ST6_gr5,
+                                                                             WL_Y_0_ST6_gr5, WL_Y_1_ST6_gr5, AH_R_0_ST6_gr5,
+                                                                             AH_R_1_ST6_gr5, AL_R_0_ST6_gr5, AL_R_1_ST6_gr5,
+                                                                             UpperMonitorRange_ST6_gr5, LowerMonitorRange_ST6_gr5);
+                           string[] bckgrd_st6_gr7 = visualEffects_ST6.LimitBrush_0(rtrn_prm_st6_gr7, WH_Y_0_ST6_gr1, WH_Y_1_ST6_gr1,
+                                                                             WL_Y_0_ST6_gr1, WL_Y_1_ST6_gr1, AH_R_0_ST6_gr1,
+                                                                             AH_R_1_ST6_gr1, AL_R_0_ST6_gr1, AL_R_1_ST6_gr1,
+                                                                             UpperMonitorRange_ST6_gr1, LowerMonitorRange_ST6_gr1);
+
+                           prm_10MAD10CY011 = rtrn_prm_st6_gr0[0];
+                           prm_10MAD10CY012 = rtrn_prm_st6_gr0[2];
+                           prm_10MAD10CG010 = rtrn_prm_st6_gr1[0];
+                           prm_10MAD10CG011 = rtrn_prm_st6_gr1[1];
+                           prm_10MAD20CG010 = rtrn_prm_st6_gr5[0];
+                           prm_10MAD10CG012 = rtrn_prm_st6_gr7[0];
+
+                           bckgrd_10MAD10CY011 = bckgrd_st6_gr0[0];
+                           bckgrd_10MAD10CY012 = bckgrd_st6_gr0[2];
+                           bckgrd_10MAD10CG010 = bckgrd_st6_gr1[0];
+                           bckgrd_10MAD10CG011 = bckgrd_st6_gr1[1];
+                           bckgrd_10MAD20CG010 = bckgrd_st6_gr5[0];
+                           bckgrd_10MAD10CG012 = bckgrd_st6_gr7[0];
+
+
+
+                       }
+                       else
+                       {
+                           modbusClient_ST6.Disconnect();
+                           timer_ST6.Stop();
+                           tbl_Status_ST6 = "Отключено";
+                           sb_Bckgrnd_ST6 = "Coral";
+                           bt_ConAct_ST6 = true;
+                           tb_IPAdrrAct_ST6 = true;
+                           bt_DisconAct_ST6 = false;
+
+                       }
+                   };
+                    timer_ST6.Start();
+                }
+                else
+                {
+                    return;
+                }
 
             });
 
