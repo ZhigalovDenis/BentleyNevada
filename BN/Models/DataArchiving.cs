4f21ddfc9fa3cf72fdca8f4233418579ef34ab7f
Денис Жигалов <dzhig001@gmail.com>
2022-07-21 13:26:08 +0500

    В класс DataRchiving добавлен метод проверки свободного места на диске CheckFreeSpaceOnDisk.

--- BN/Models/DataArchiving.cs
+++ BN/Models/DataArchiving.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BN.Models
 {
     internal class DataArchiving
     {
+        private readonly DriveInfo[] allDrives = DriveInfo.GetDrives();
+        private readonly string CurDir = Directory.GetCurrentDirectory();
+        private const int AllowedFreeSpace = 1000; // Размер в Мб
+
+        public bool CheckFreeSpaceOnDisk()
+        {
+            string DiskName = CurDir.Substring(0, 3);
+            bool IsCkecked = false;
+            foreach (var disk in allDrives)
+            {
+                if (disk.IsReady == true) //Проверяем готов ли диск
+                {
+                    if (disk.Name == DiskName) // Проверяем кол-во свободного места на диске 
+                    {
+                        long FreeSpaceInMB = (disk.TotalFreeSpace / 1024) / 1024;
+                        if (FreeSpaceInMB > AllowedFreeSpace)
+                        {
+                           //Все хорошо места достаточно
+                            IsCkecked = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Архивация не возможна, на диске меньше 1 Гб свободно пространстав");
+                            IsCkecked = false;
+                        }
+                    }
+                }
+            }
+            return IsCkecked;
+        }
     }
 }
